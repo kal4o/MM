b9e7045021a7a1678c6c6a5884ee77189d5c1c12
Kalin Iliev <kalinaki78@abv.bg>
2020-02-12 14:47:59 +0200

    if statement optimization

--- MM/src/Method.java
+++ MM/src/Method.java
@@ -24,13 +24,13 @@ public class Method {
 					for (int j = 0; j<N*10; j++) { 
 						
 						
-						if(i+j==N || i+j==N*3 || i+j==6*N || i+j==8*N || i-j==-N || i-j==-N*3 || i-j==-N*6 || i-j==-N*8 ) {
-							System.out.print("*");
-						} //1st starline
-						else if(i+j==2*N-1 || i+j==4*N-1 || i+j==7*N-1 || i+j==9*N-1 || j-i==2*N-1 || j-i==4*N-1 || j-i==7*N-1 || j-i==9*N-1 ) {
-							System.out.print("*");
-						}  //last starline
-						else if(i+j > N && i+j < 2*N-1 || i+j > 6*N && i+j < 7*N-1 || i+j > 3*N && i+j < 4*N-1 || i+j > 8*N && i+j < 9*N-1  || j-i > N && j-i < 2*N-1 || j-i > 6*N && j-i < 7*N-1 || j-i > 3*N && j-i < 4*N-1 || j-i > 8*N && j-i < 9*N-1 ) {
+//						if(i+j==N || i+j==N*3 || i+j==6*N || i+j==8*N || i-j==-N || i-j==-N*3 || i-j==-N*6 || i-j==-N*8 ) {
+//							System.out.print("*");
+//						} //1st starline
+//						else if(i+j==2*N-1 || i+j==4*N-1 || i+j==7*N-1 || i+j==9*N-1 || j-i==2*N-1 || j-i==4*N-1 || j-i==7*N-1 || j-i==9*N-1 ) {
+//							System.out.print("*");
+//						}  //last starline
+						 if(i+j >= N && i+j <= 2*N-1 || i+j >= 6*N && i+j <= 7*N-1 || i+j >= 3*N && i+j <= 4*N-1 || i+j >= 8*N && i+j <= 9*N-1  || j-i >= N && j-i <= 2*N-1 || j-i >= 6*N && j-i <= 7*N-1 || j-i >= 3*N && j-i <= 4*N-1 || j-i >= 8*N && j-i <= 9*N-1 ) {
 							System.out.print("*"); //inner starline
 						} else {
 							System.out.print("-");
